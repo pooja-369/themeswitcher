675dcb0871cb0179b68afaa4318073459959da3f
pooja-369 <pujagoyal369@gmail.com>
2024-06-20 22:20:19 +0530

    theme works

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,17 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import Card from './components/Card'
+import ThemeButtons from './components/ThemeButtons'
+import { ThemeProvider } from './context/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const[themeMode,setThemeMode]=useState("light")
+
+    const lightTheme=()=>{
+      setThemeMode("light")
+    }
+    const darkTheme=()=>{
+      setThemeMode("dark")
+    }
+
+
+    //actual change in theme
+
+    useEffect(()=>{
+      document.querySelector('html').classList.remove("light","dark")
+      document.querySelector('html').classList.add(themeMode)
+    },[themeMode])
+    
 
   return (
-    <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* theme button */}
+                        <ThemeButtons/>
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       {/* Card */}
+                       <Card/>
+                    </div>
+                </div>
+            </div>
+            </ThemeProvider>
   )
 }
 
